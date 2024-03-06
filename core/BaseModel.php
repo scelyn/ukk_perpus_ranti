d2cec9a09d45d9c3f9cbd415c4ab37c1f5bf84c1
@sclyn <159868695+scelyn@users.noreply.github.com>
2024-03-05 21:04:09 -0800

    Update BaseModel.php

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -6,7 +6,7 @@ class BaseModel
 
 	public function __construct()
 	{
-		$this->mysqli = new mysqli('localhost', 'root', '', 'perpus_digital');
+		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk_perpus_ranti');
 	}
 
 	public function getByUsername($username)
